e49c37bcd4db937416fccf3cfa5fd1df4b83574e
Ian McBurnie <ianmcburnie@hotmail.com>
2016-09-21 15:23:28 -0700

    Just renamed a few vars

--- jquery.menu.js
+++ jquery.menu.js
@@ -1,7 +1,7 @@
 /**
 * @file jQuery plugin that creates the basic interactivity for an ARIA menu widget
 * @author Ian McBurnie <ianmcburnie@hotmail.com>
-* @version 0.2.4
+* @version 0.2.5
 * @requires jquery
 * @requires jquery-next-id
 * @requires jquery-button-flyout
@@ -31,16 +31,16 @@
         options = options || {};
 
         return this.each(function onEach() {
-            var $this = $(this);
-            var $button = $this.find('button');
-            var $rootMenu = $this.find('> [role=menu], > *:last-child > [role=menu]').first();
+            var $widget = $(this);
+            var $button = $widget.find('button');
+            var $rootMenu = $widget.find('> [role=menu], > *:last-child > [role=menu]').first();
             var $groups = $rootMenu.find('> div[role=presentation]');
-            var $allmenuitems = $rootMenu.find('> [role^=menuitem], > div > [role^=menuitem], > a');
+            var $allMenuItems = $rootMenu.find('> [role^=menuitem], > div > [role^=menuitem], > a');
             var $links = $rootMenu.find('a');
             var $buttons = $rootMenu.find('[role=menuitem]');
             var $checkboxes = $rootMenu.find('[role=menuitemcheckbox]');
             var $radios = $rootMenu.find('[role=menuitemradio]');
-            var $firstMenuItem = $allmenuitems.first();
+            var $firstMenuItem = $allMenuItems.first();
             var $subMenus = $rootMenu.find('[role=menuitem][aria-haspopup=true]');
             var keyCodeMap = createKeyCodeMap();
             var isSupportShortcutKey = !options.disableShortcutKey;
@@ -48,7 +48,7 @@
 
             // store first char of all menu items
             if (isSupportShortcutKey) {
-                $allmenuitems.each(function(idx) {
+                $allMenuItems.each(function(idx) {
                     var firstChar = $(this).text()[0];
                     if (!shortcutKeyMap[firstChar]) {
                         shortcutKeyMap[firstChar] = idx;
@@ -57,10 +57,10 @@
             }
 
             // assign id to widget
-            $this.nextId('popupmenu');
+            $widget.nextId('popupmenu');
 
             // menu is built on top of button-flyout plugin
-            $this.buttonFlyout({focusManagement: true});
+            $widget.buttonFlyout({focusManagement: true});
 
             // listen for specific key presses on all menu items
             $rootMenu.commonKeyDown();
@@ -69,62 +69,62 @@
             $rootMenu.rovingTabindex('[role^=menuitem]', {axis: 'y'});
 
             // assign id to menu
-            $rootMenu.prop('id', $this.prop('id') + '-menu');
+            $rootMenu.prop('id', $widget.prop('id') + '-menu');
 
             // create popup menu semantics
             $button
                 .attr('aria-haspopup', 'true')
-                .prop('id', $this.prop('id') + '-button');
+                .prop('id', $widget.prop('id') + '-button');
 
             // all submenus start in collapsed state
             $subMenus.attr('aria-expanded', 'false');
 
             $rootMenu.on('click spaceKeyDown enterKeyDown', function(e) {
-                var $target = $(e.originalEvent.target);
-                var role = $target.attr('role');
+                var $menuitem = $(e.originalEvent.target);
+                var role = $menuitem.attr('role');
 
                 switch (role) {
                     case "menuitemradio":
                         $radios.attr('aria-checked', 'false');
-                        $target.attr('aria-checked', 'true');
+                        $menuitem.attr('aria-checked', 'true');
                         break;
                     case "menuitemcheckbox":
-                        $target.attr('aria-checked', $target.attr('aria-checked') === 'true' ? 'false' : 'true');
+                        $menuitem.attr('aria-checked', $menuitem.attr('aria-checked') === 'true' ? 'false' : 'true');
                         break;
                     default:
                         break;
                 }
 
-                $this.trigger('menuSelect');
+                $widget.trigger('menuSelect');
             });
 
             // reset all tabindexes when flyout opens and closes
-            $this.on('buttonFlyoutOpen buttonFlyoutClose', function onShowOrHide() {
-                $allmenuitems.attr('tabindex', '-1');
+            $widget.on('buttonFlyoutOpen buttonFlyoutClose', function onShowOrHide() {
+                $allMenuItems.attr('tabindex', '-1');
             });
 
             // if char key is pressed, set focus on 1st matching menu item
             if (isSupportShortcutKey) {
-                $allmenuitems.on('keydown', function(e) {
+                $allMenuItems.on('keydown', function(e) {
                     var char = keyCodeMap[e.keyCode];
                     var itemIndex = shortcutKeyMap[char];
 
                     if (itemIndex) {
-                        $allmenuitems.get(itemIndex).focus();
+                        $allMenuItems.get(itemIndex).focus();
                     }
                 });
             }
 
             // when a menu item selection has been made, set focus back to button
-            $this.on('menuSelect', function onMenuSelect(e) {
+            $widget.on('menuSelect', function onMenuSelect(e) {
                 $button.focus();
             });
 
             // use a plugin to prevent page scroll when arrow keys are pressed
-            $this.preventScrollKeys('[role^=menuitem]');
+            $widget.preventScrollKeys('[role^=menuitem]');
 
             // mark widget as js initialised
-            $this.addClass('menu--js');
+            $widget.addClass('menu--js');
         });
     };
 }(jQuery));
